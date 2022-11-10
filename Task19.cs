755ec0fe13d265e17bab8c082a2b670d2995d831
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-10 21:54:08 +0600

    Update Task19.cs

--- Task19.cs
+++ Task19.cs
@@ -1,1 +1,13 @@
+Console.WriteLine("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine());
+int FirstDigit = n/10000;
+int SecondDigit =(n / 1000) % 10;
+int FourthDigit =(n / 10) % 10;
+int LastDigit = n % 10;
+  if ((FirstDigit == LastDigit)&&(SecondDigit == FourthDigit))
+  Console.WriteLine("Это число - палиндром");
+  else
+  {
+   Console.WriteLine("Это число - не палиндром"); 
+  }
 
