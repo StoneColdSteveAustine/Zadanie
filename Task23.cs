3a297bfe8b3d5c7ac8f6e3fec9f65a267a0ce30a
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-10 21:56:41 +0600

    Update Task23.cs

--- Task23.cs
+++ Task23.cs
@@ -1,1 +1,25 @@
+Console.WriteLine("Введите число");
+int cube = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+void Cube(int[] Cube){
+  int Count = 0;
+  int Length = Cube.Length;
+  while (Count < Length)
+  {
+    Cube[Count] = Convert.ToInt32(Math.Pow(Count, 3));
+    Count++;
+  }
+}
 
+void PrintArry(int[] Collection){
+  int Count = Collection.Length;
+  int index = 1;
+  while(index < Count){
+        Console.WriteLine(Collection[index]+ " ");
+    index++;
+  }
+} 
+
+int[] array = new int[cube+1];
+Cube(array);
+PrintArry(array);
